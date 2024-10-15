b5a31e3520c80c8cc8555651674ba742a7ea29e7
tranthaiCT <tranthai2k4@gmail.com>
2024-10-15 18:53:34 +0700

    api

--- src/main/java/com/project/sneaker/controller/DotGiamGiaController.java
+++ src/main/java/com/project/sneaker/controller/DotGiamGiaController.java
@@ -2,6 +2,7 @@ package com.project.sneaker.controller;
 
 import com.project.sneaker.entity.dotgiamgia.DotGiamGia;
 import com.project.sneaker.entity.dotgiamgia.DotGiamGiaReponse;
+import com.project.sneaker.entity.dotgiamgia.DotGiamGiaRequets;
 import com.project.sneaker.entity.dotgiamgia.TaiKhoan;
 import com.project.sneaker.repository.DotGiamGiaRepo;
 import com.project.sneaker.repository.TaiKhoanRepo;
@@ -9,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +23,60 @@ public class DotGiamGiaController {
     TaiKhoanRepo taiKhoanRepo;
 
     @GetMapping("/list")
-    public List<DotGiamGiaReponse> hienThi(){
+    public List<DotGiamGiaReponse> hienThi() {
         return dotGiamGiaRepo.getAll();
     }
 
     @GetMapping("/phantrang")
-   public List<DotGiamGiaReponse> trang(Pageable pageable){
+    public List<DotGiamGiaReponse> trang(Pageable pageable) {
         return dotGiamGiaRepo.phantrang(pageable).getContent();
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestBody DotGiamGiaRequets requets) {
+        DotGiamGia dotGiamGia = new DotGiamGia();
+        dotGiamGia.setTenDot(requets.getTenDot());
+        dotGiamGia.setGiaTri(requets.getGiaTri());
+        dotGiamGia.setLoaiGiamGia(requets.getLoaiGiamGia());
+        dotGiamGia.setTrangThai(requets.getTrangThai());
+        dotGiamGia.setThoiGianBatDau(requets.getThoiGianBatDau());
+        dotGiamGia.setThoiGianKetThuc(requets.getThoiGianKetThuc());
+        dotGiamGia.setNgayTao(requets.getNgayTao());
+        dotGiamGia.setTaiKhoan(requets.getTaiKhoan());
+        DotGiamGia dgg = dotGiamGiaRepo.save(dotGiamGia);
+        if (dgg == null) {
+            return "them ko thanh cong";
+        }
+        return "them thanh cong";
+    }
+    @PutMapping("/update")
+    public String update(@RequestBody DotGiamGiaRequets requets) {
+        DotGiamGia dotGiamGia = new DotGiamGia();
+        dotGiamGia.setId(requets.getId());
+        dotGiamGia.setTenDot(requets.getTenDot());
+        dotGiamGia.setGiaTri(requets.getGiaTri());
+        dotGiamGia.setLoaiGiamGia(requets.getLoaiGiamGia());
+        dotGiamGia.setTrangThai(requets.getTrangThai());
+        dotGiamGia.setThoiGianBatDau(requets.getThoiGianBatDau());
+        dotGiamGia.setThoiGianKetThuc(requets.getThoiGianKetThuc());
+        dotGiamGia.setNgayTao(requets.getNgayTao());
+        dotGiamGia.setTaiKhoan(requets.getTaiKhoan());
+        DotGiamGia dgg = dotGiamGiaRepo.save(dotGiamGia);
+        if (dgg == null) {
+            return "sua ko thanh cong";
+        }
+        return "sua thanh cong";
+    }
+
+
+    @GetMapping("/detail")
+    public DotGiamGia detail(@RequestParam(value = "id", defaultValue = "1") long id) {
+        return dotGiamGiaRepo.findById(id).get();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable long id){
+        dotGiamGiaRepo.deleteById(id);
+        return "xoa thanh cong";
     }
 }

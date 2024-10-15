24391f46f05604bcbd7256b8e4ff6fe4d45f8c57
tranthaiCT <tranthai2k4@gmail.com>
2024-10-15 07:11:07 +0700

    tao file dot giam gia

--- src/main/java/com/project/sneaker/entity/dotgiamgia/DotGiamGia.java
+++ src/main/java/com/project/sneaker/entity/dotgiamgia/DotGiamGia.java
@@ -1,4 +1,11 @@
 package com.project.sneaker.entity.dotgiamgia;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+
 public class DotGiamGia {
 }
---
+++ src/main/java/com/project/sneaker/repository/DotGiamGiaRepo.java
@@ -0,0 +1,4 @@
+package com.project.sneaker.repository;
+
+public interface DotGiamGiaRepo {
+}
